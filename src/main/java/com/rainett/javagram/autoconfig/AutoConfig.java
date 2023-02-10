75f388ec1b226b8ef92312ee78232f854b87de25
rainett <96131229+rainett@users.noreply.github.com>
2023-02-10 21:02:40 +0200

    reduced autoconfig

--- src/main/java/com/rainett/javagram/autoconfig/AutoConfig.java
+++ src/main/java/com/rainett/javagram/autoconfig/AutoConfig.java
@@ -1,22 +1,31 @@
 package com.rainett.javagram.autoconfig;
 
+import com.rainett.javagram.config.AppConfig;
 import com.rainett.javagram.config.BotConfig;
-import com.rainett.javagram.controller.processor.UpdateProcessor;
-import com.rainett.javagram.controller.processor.UpdateProcessorImpl;
-import com.rainett.javagram.controller.webhook.WebhookBot;
 import com.rainett.javagram.controller.executables.container.ExecutablesContainer;
 import com.rainett.javagram.controller.executables.container.ExecutablesContainerImpl;
 import com.rainett.javagram.controller.executor.BotExecutor;
 import com.rainett.javagram.controller.executor.BotExecutorImpl;
+import com.rainett.javagram.controller.processor.UpdateProcessor;
+import com.rainett.javagram.controller.processor.UpdateProcessorImpl;
+import com.rainett.javagram.controller.webhook.WebhookBot;
 import com.rainett.javagram.controller.webhook.WebhookController;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 
 @Configuration
-public class ControllerAutoConfig {
+@EnableConfigurationProperties(BotConfig.class)
+public class AutoConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AppConfig appConfig() {
+        return new AppConfig();
+    }
 
     @Bean
     @ConditionalOnMissingBean
@@ -38,8 +47,8 @@ public class ControllerAutoConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    public SetWebhook setWebhook() {
-        return new SetWebhook();
+    public SetWebhook setWebhook(BotConfig botConfig) {
+        return SetWebhook.builder().url(botConfig.getPath()).build();
     }
 
     @Bean
--- src/main/java/com/rainett/javagram/config/AppConfig.java
+++ src/main/java/com/rainett/javagram/config/AppConfig.java
@@ -2,25 +2,11 @@ package com.rainett.javagram.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 
 import java.util.Locale;
 
 public class AppConfig {
-
-    private final BotConfig botConfig;
-
-    public AppConfig(BotConfig botConfig) {
-        this.botConfig = botConfig;
-    }
-
-    @Bean
-    public SetWebhook setWebhook() {
-        return SetWebhook.builder().url(botConfig.getPath()).build();
-    }
-
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
